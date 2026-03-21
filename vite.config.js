d3e6fb64bc5c9634efea16196cbf994362de1ba2
dhairydobariya <dhairydobariya@gmail.com>
2026-03-21 19:23:24 +0530

    m

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: process.env.GITHUB_PAGES === 'true' ? '/gk2/' : '/',
   plugins: [react()],
+  build: {
+    outDir: process.env.GITHUB_PAGES === 'true' ? '../docs' : 'dist',
+  },
   test: {
     globals: true,
     environment: 'jsdom',
